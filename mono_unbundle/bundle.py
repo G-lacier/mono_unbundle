8938489a6df87b0f2c41c37023e37e4cfd9856a0
G-lacier <130880758+G-lacier@users.noreply.github.com>
2025-05-01 03:20:32 -0500

    Update bundle.py

--- mono_unbundle/bundle.py
+++ mono_unbundle/bundle.py
@@ -1,7 +1,7 @@
 from elftools.elf.elffile import ELFFile
 from io import BufferedReader
 from pathlib import Path
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 import gzip
 import magic
 import sys
@@ -24,63 +24,66 @@ class MonoBundle:
         self.dlls = self.__parse_elf()
 
     def __del__(self):
-        if self.file is not None:
-            self.file.close()
+        try:
+            if self.file is not None:
+                self.file.close()
+        except Exception:
+            pass
 
     def __open_elf(self, file: Union[str, BufferedReader]) -> Tuple[BufferedReader, ELFFile]:
-        f = file if type(file) is BufferedReader else open(file, 'rb')
-        m = magic.from_buffer(f.read(1024), mime=False)
+        f = file if isinstance(file, BufferedReader) else open(file, 'rb')
+        magic_bytes = f.read(1024)
+        f.seek(0)
+        m = magic.from_buffer(magic_bytes, mime=False)
 
         if m.startswith('PE32 '):
             raise NotImplementedError("PE bundles are not currently supported.")
         elif not m.startswith('ELF '):
             raise IOError("File is not an ELF library.")
-        self.little_endian = "LSB" in m
 
+        self.little_endian = "LSB" in m
         return f, ELFFile(f)
 
-    def __parse_elf(self) -> [MonoDll]:
-        # Shout out to maldrolyzer for independently arriving at the same solution
-        # (https://github.com/maldroid/maldrolyzer/blob/master/plugins/z3core.py#L48)
-
+    def __parse_elf(self) -> List[MonoDll]:
         section = self.elf_file.get_section_by_name('.dynsym')
+        if section is None:
+            raise ValueError("Missing .dynsym section in ELF.")
+
         dlls = []
 
         for symbol in section.iter_symbols():
             if symbol['st_shndx'] != 'SHN_UNDEF' and symbol.name.startswith('assembly_data_'):
-                dll = dict()
-                dll['name'] = symbol.name[14:].replace('_dll', '.dll')
-                dll['offset'] = symbol['st_value']
-                dll['size'] = symbol['st_size']
-                dll = MonoDll(dll['name'], dll['offset'], dll['size'])
+                name = symbol.name[14:].replace('_dll', '.dll')
+                dll = MonoDll(name, symbol['st_value'], symbol['st_size'])
 
                 if self.verbose:
-                    print("found %s" % dll.name, file=sys.stderr)
+                    print(f"found {dll.name}", file=sys.stderr)
 
                 dlls.append(dll)
 
         return dlls
 
-    def get_dlls(self) -> [MonoDll]:
+    def get_dlls(self) -> List[MonoDll]:
         return self.dlls
 
     def extract(self, dll: MonoDll, path: str = None) -> bytes:
         self.file.seek(dll.offset)
-        d = gzip.decompress(self.file.read(dll.size))
+        data = gzip.decompress(self.file.read(dll.size))
 
         if path is not None:
             p = Path(path)
             if p.exists() and not p.is_dir():
                 raise IOError("PATH is not a directory")
             p.mkdir(parents=True, exist_ok=True)
-            outfile = open(p.joinpath(dll.name), 'wb')
-            outfile.write(d)
-            outfile.close()
 
-        return d
+            out_path = p / dll.name
+            with open(out_path, 'wb') as outfile:
+                outfile.write(data)
+
+        return data
 
     def extract_all(self):
         for dll in self.dlls:
             self.extract(dll, self.path)
             if self.verbose:
-                print("extracted %s" % dll.name)
+                print(f"extracted {dll.name}", file=sys.stderr)
